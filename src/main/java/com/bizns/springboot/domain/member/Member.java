d044b0e2320d9d3de4ab53466e4ef4bc263869d1
Timo <developer.timo@gmail.com>
2021-03-22 17:51:21 +0900

    Member 도메인 및 테스트 코드 작성

---
+++ src/main/java/com/bizns/springboot/domain/member/Member.java
@@ -0,0 +1,25 @@
+package com.bizns.springboot.domain.member;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Builder
+    public Member(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/com/bizns/springboot/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.bizns.springboot.domain.member;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @After
+    public void cleanup() {
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String name = "테스터";
+
+        memberRepository.save(Member.builder()
+                .name(name)
+                .build());
+
+        //when
+        List<Member> members = memberRepository.findAll();
+
+        //then
+        Member member = members.get(0);
+        assertThat(member.getName()).isEqualTo(name);
+    }
+}

bfd8718d73080d7cd000a382d569d7b8dca2abb5
Timo <developer.timo@gmail.com>
2021-03-22 21:55:47 +0900

    타임리프 설정

--- src/main/java/com/bizns/bizneyland/web/controller/IndexController.java
+++ src/main/java/com/bizns/bizneyland/web/controller/IndexController.java
@@ -1,13 +1,27 @@
 package com.bizns.bizneyland.web.controller;
 
+import com.bizns.bizneyland.config.auth.LoginUser;
+import com.bizns.bizneyland.config.auth.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public void index() {
+    private final HttpSession httpSession;
 
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";
     }
 }

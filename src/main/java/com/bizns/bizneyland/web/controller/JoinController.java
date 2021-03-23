c5024872d244129a2d9c25fea0cc31ec62e615d5
Timo <developer.timo@gmail.com>
2021-03-23 13:06:02 +0900

    회원 등록 화면 및 기능 추가

--- src/main/java/com/bizns/bizneyland/web/controller/JoinController.java
+++ src/main/java/com/bizns/bizneyland/web/controller/JoinController.java
@@ -1,7 +1,10 @@
 package com.bizns.bizneyland.web.controller;
 
 import com.bizns.bizneyland.service.CompanyService;
+import com.bizns.bizneyland.service.MemberService;
 import com.bizns.bizneyland.web.dto.CompanyRequestDto;
+import com.bizns.bizneyland.web.dto.MemberResponseDto;
+import com.bizns.bizneyland.web.dto.MemberSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JoinController {
 
     private final CompanyService companyService;
+    private final MemberService memberService;
 
     /**
      * 회사 등록 화면
@@ -32,6 +36,23 @@ public class JoinController {
         companyService.save(requestDto);
 
         return "redirect:/company";
+    }
+
+    /**
+     * 회원 등록 화면
+     * */
+    @GetMapping("member")
+    public String memberForm() {
+        return "join/formMember";
+    }
+
+    /**
+     * 회원 등록
+     * */
+    @PostMapping("member")
+    public String registerMember(MemberSaveRequestDto requestDto) {
+        memberService.save(requestDto);
+        return "redirect:/member";
     }
 
 }
--- src/main/java/com/bizns/bizneyland/web/dto/MemberSaveRequestDto.java
+++ src/main/java/com/bizns/bizneyland/web/dto/MemberSaveRequestDto.java
@@ -4,8 +4,9 @@ import com.bizns.bizneyland.domain.member.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Getter
+@Getter @Setter
 @NoArgsConstructor
 public class MemberSaveRequestDto {
 

edc28688c34f25be5e0c9469ea2746124044b531
Timo <developer.timo@gmail.com>
2021-03-23 15:48:14 +0900

    로그인 세션 어노테이션으로 리팩토링

--- src/main/java/com/bizns/bizneyland/web/controller/IndexController.java
+++ src/main/java/com/bizns/bizneyland/web/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.bizns.bizneyland.web.controller;
 
+import com.bizns.bizneyland.config.auth.LoginUser;
 import com.bizns.bizneyland.config.auth.dto.SessionUser;
 import com.bizns.bizneyland.service.CompanyService;
 import com.bizns.bizneyland.service.MemberService;
@@ -8,19 +9,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private final HttpSession httpSession;
     private final CompanyService companyService;
     private final MemberService memberService;
 
     @GetMapping("/")
-    public String index(Model model) {
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    public String index(@LoginUser SessionUser user, Model model) {
         if (user != null) {
             model.addAttribute("user", user);
         }

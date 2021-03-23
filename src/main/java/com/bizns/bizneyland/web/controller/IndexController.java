79ecc1b6cf32a9a6ef79ac17f6dee0827889c05f
Timo <developer.timo@gmail.com>
2021-03-23 15:33:53 +0900

    구글 로그인을 위한 설정 추가

--- src/main/java/com/bizns/bizneyland/web/controller/IndexController.java
+++ src/main/java/com/bizns/bizneyland/web/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.bizns.bizneyland.web.controller;
 
+import com.bizns.bizneyland.config.auth.dto.SessionUser;
 import com.bizns.bizneyland.service.CompanyService;
 import com.bizns.bizneyland.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -7,15 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final HttpSession httpSession;
     private final CompanyService companyService;
     private final MemberService memberService;
 
     @GetMapping("/")
     public String index(Model model) {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
         return "main";
     }
 

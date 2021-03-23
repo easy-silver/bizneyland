7a411d04e57a9aa4ca40ac4e069fc738b3060f99
Timo <developer.timo@gmail.com>
2021-03-23 16:26:50 +0900

    테스트 코드 모두 통과되도록 수정함

---
+++ src/main/java/com/bizns/bizneyland/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.bizns.bizneyland.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}

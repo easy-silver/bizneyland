63bf76945e8b7e69b059812b8d7a2ccd7bbfe2d4
Timo <developer.timo@gmail.com>
2021-03-23 10:13:29 +0900

    부트스트랩 테마 화면 추가(레이아웃 의존성 등록)

--- src/main/java/com/bizns/bizneyland/web/controller/IndexController.java
+++ src/main/java/com/bizns/bizneyland/web/controller/IndexController.java
@@ -1,27 +1,22 @@
 package com.bizns.bizneyland.web.controller;
 
-import com.bizns.bizneyland.config.auth.LoginUser;
-import com.bizns.bizneyland.config.auth.SessionUser;
+import com.bizns.bizneyland.service.CompanyService;
+import com.bizns.bizneyland.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private final HttpSession httpSession;
+    private final CompanyService companyService;
+    private final MemberService memberService;
 
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user) {
-
-        if (user != null) {
-            model.addAttribute("userName", user.getName());
-        }
-
+    public String index(Model model) {
         return "index";
     }
+
 }

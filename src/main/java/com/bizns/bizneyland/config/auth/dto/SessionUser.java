29eda75eaa0cac4976aaa4b38aa5b530474a21b6
Timo <developer.timo@gmail.com>
2021-03-29 19:10:46 +0900

    세션 정보에 user_seq 추가

--- src/main/java/com/bizns/bizneyland/config/auth/dto/SessionUser.java
+++ src/main/java/com/bizns/bizneyland/config/auth/dto/SessionUser.java
@@ -1,5 +1,6 @@
 package com.bizns.bizneyland.config.auth.dto;
 
+import com.bizns.bizneyland.domain.member.Member;
 import com.bizns.bizneyland.domain.user.User;
 import lombok.Getter;
 
@@ -10,10 +11,12 @@ public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
+    private Long userSeq;
 
     public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
+        this.userSeq = user.getUserSeq();
     }
 }

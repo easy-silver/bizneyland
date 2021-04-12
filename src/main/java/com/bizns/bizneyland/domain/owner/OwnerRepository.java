a20ea6fc7e80ec1d4beef3a2dd317c5983d25934
Timo <developer.timo@gmail.com>
2021-04-12 10:48:37 +0900

    고객 대표자 Dto 및 조회 로직 추가

--- src/main/java/com/bizns/bizneyland/domain/owner/OwnerRepository.java
+++ src/main/java/com/bizns/bizneyland/domain/owner/OwnerRepository.java
@@ -1,7 +1,13 @@
 package com.bizns.bizneyland.domain.owner;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Arrays;
+import java.util.List;
 
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
+    @Query("SELECT o FROM Owner o WHERE o.client.clientSeq = ?1")
+    List<Owner> findAllByClientSeq(Long clientSeq);
 }
--- src/main/java/com/bizns/bizneyland/service/OwnerService.java
+++ src/main/java/com/bizns/bizneyland/service/OwnerService.java
@@ -2,9 +2,13 @@ package com.bizns.bizneyland.service;
 
 import com.bizns.bizneyland.domain.owner.OwnerRepository;
 import com.bizns.bizneyland.web.dto.OwnerRequestDto;
+import com.bizns.bizneyland.web.dto.OwnerResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -14,6 +18,14 @@ public class OwnerService {
     @Transactional
     public Long save(OwnerRequestDto requestDto) {
         return repository.save(requestDto.toEntity()).getOwnerSeq();
+    }
+
+    @Transactional
+    public List<OwnerResponseDto> findAllByClientSeq(Long clientSeq) {
+        return repository.findAllByClientSeq(clientSeq)
+                .stream()
+                .map(OwnerResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }

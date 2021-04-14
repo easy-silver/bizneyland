39e4315dfd0ab094e5fe947970f034bbc4dceaf3
Timo <developer.timo@gmail.com>
2021-04-14 16:12:15 +0900

    TM 등록 폼 뷰 재배치

--- src/main/java/com/bizns/bizneyland/web/controller/TmController.java
+++ src/main/java/com/bizns/bizneyland/web/controller/TmController.java
@@ -7,9 +7,11 @@ import com.bizns.bizneyland.domain.client.ClientRepository;
 import com.bizns.bizneyland.domain.member.Member;
 import com.bizns.bizneyland.domain.member.MemberRepository;
 import com.bizns.bizneyland.service.ClientService;
-import com.bizns.bizneyland.service.OwnerService;
 import com.bizns.bizneyland.service.TmService;
-import com.bizns.bizneyland.web.dto.*;
+import com.bizns.bizneyland.web.dto.ClientRequestDto;
+import com.bizns.bizneyland.web.dto.OwnerRequestDto;
+import com.bizns.bizneyland.web.dto.TmRequestDto;
+import com.bizns.bizneyland.web.dto.TmResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,22 +25,43 @@ public class TmController {
 
     private final ClientService clientService;
     private final ClientRepository clientRepository;
-    private final OwnerService ownerService;
     private final TmService tmService;
     private final MemberRepository memberRepository;
 
+    /**
+     * TM 상담 목록
+     */
     @GetMapping("list")
     public void list(Model model) {
         model.addAttribute("tmList", tmService.findAllDesc());
     }
 
-    @GetMapping("form")
-    public String clientForm() {
-        return "tm/registerClient";
+    /**
+     * TM 상세
+     */
+    @GetMapping("detail/{tmNo}")
+    public String tmDetail(Model model, @PathVariable Long tmNo) {
+        TmResponseDto tm = tmService.findById(tmNo);
+        Long clientNo = tm.getClientSeq();
+        clientService.findById(clientNo);
+        model.addAttribute("tm", tm);
+        model.addAttribute("client", clientService.findById(clientNo));
+        return "tm/detail";
     }
 
-    @PostMapping("registerClient")
-    public String registerClient(ClientRequestDto clientDto, OwnerRequestDto ownerDto, RedirectAttributes redirectAttributes) {
+    /**
+     * TM 등록(업체) 양식
+     */
+    @GetMapping("register/client")
+    public String registerClient() {
+        return "tm/register/client";
+    }
+
+    /**
+     * TM 등록 - 업체 등록
+     */
+    @PostMapping("register/client")
+    public String registerClient(ClientRequestDto clientDto, RedirectAttributes redirectAttributes) {
 
         Client client;
 
@@ -49,23 +72,25 @@ public class TmController {
             client = clientService.save(clientDto);
         }
 
-        // INSERT OWNER
-        ownerDto.updateClient(client);
-        ownerService.save(ownerDto);
-
         redirectAttributes.addAttribute("clientSeq", client.getClientSeq());
 
         return "redirect:/tm/form/tmInfo";
     }
 
-    @GetMapping("form/tmInfo")
+    /**
+     * TM 등록(상담 정보) 양식
+     */
+    @GetMapping("register/tmInfo")
     public String tmForm(@ModelAttribute("clientSeq") Long clientSeq, Model model) {
         model.addAttribute("clientSeq", clientSeq);
 
-        return "tm/registerTmInfo";
+        return "tm/register/tmInfo";
     }
 
-    @PostMapping("registerTmInfo")
+    /**
+     * TM 등록(상담 정보) 등록
+     */
+    @PostMapping("register/tmInfo")
     public String registerTmInfo(TmRequestDto requestDto, @LoginUser SessionUser user) {
 
         // 세션에 있는 USER_SEQ로 Member 찾기
@@ -82,24 +107,20 @@ public class TmController {
         return "redirect:/tm/list";
     }
 
-    @GetMapping("form/sales")
-    public String salesForm() {
-        return "tm/registerSales";
+    /**
+     * TM 등록(매출 정보) 양식
+     */
+    @GetMapping("register/sales")
+    public String registerSales(Model model) {
+        return "tm/register/sales";
     }
 
-    @PostMapping("registerSales")
+    /**
+     * TM 등록(매출 정보) 등록
+     */
+    @PostMapping("register/sales")
     public String registerSales() {
         return "redirect:/tm/list";
     }
 
-    @GetMapping("detail/{tmNo}")
-    public String tmDetail(Model model, @PathVariable Long tmNo) {
-        TmResponseDto tm = tmService.findById(tmNo);
-        Long clientNo = tm.getClientSeq();
-        clientService.findById(clientNo);
-        model.addAttribute("tm", tm);
-        model.addAttribute("client", clientService.findById(clientNo));
-        model.addAttribute("owners", ownerService.findAllByClientSeq(clientNo));
-        return "tm/detail";
-    }
 }

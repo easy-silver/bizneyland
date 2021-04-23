d1362f14284ed6dc74ff16c41710488775dc0487
Timo <developer.timo@gmail.com>
2021-04-23 21:58:58 +0900

    대표자(Owner) 엔티티, 레포지토리 추가

---
+++ src/main/java/com/bizns/bizneyland/domain/owner/Owner.java
@@ -0,0 +1,44 @@
+package com.bizns.bizneyland.domain.owner;
+
+import com.bizns.bizneyland.domain.company.Company;
+import com.bizns.bizneyland.domain.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Owner {
+
+    /* 대표자 번호(PK) */
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "owner_seq")
+    private Long id;
+
+    /* 회사 번호 */
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "company_seq", nullable = false)
+    private Company company;
+
+    /* 회원 번호 */
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "member_seq", nullable = false)
+    private Member member;
+
+    /* 대표자 이름 */
+    @Column(nullable = false)
+    private String name;
+
+    @Builder
+    public Owner(Company company, Member member, String name) {
+        this.company = company;
+        this.member = member;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/bizns/bizneyland/domain/owner/OwnerRepository.java
@@ -0,0 +1,8 @@
+package com.bizns.bizneyland.domain.owner;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+}

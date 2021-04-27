911f06b24c8dd18d69cbfc74f1715cbd4b69dd61
Timo <developer.timo@gmail.com>
2021-04-27 22:47:00 +0900

    TM 수정 화면 추가

--- src/main/java/com/bizns/bizneyland/web/controller/TmController.java
+++ src/main/java/com/bizns/bizneyland/web/controller/TmController.java
@@ -1,8 +1,5 @@
 package com.bizns.bizneyland.web.controller;
 
-import com.bizns.bizneyland.config.auth.LoginUser;
-import com.bizns.bizneyland.config.auth.dto.SessionUser;
-import com.bizns.bizneyland.domain.client.Client;
 import com.bizns.bizneyland.service.ClientService;
 import com.bizns.bizneyland.service.LoanService;
 import com.bizns.bizneyland.service.SalesService;
@@ -13,8 +10,10 @@ import com.bizns.bizneyland.web.dto.TmResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @RequestMapping("/tm")
@@ -38,7 +37,7 @@ public class TmController {
      * TM 상세
      */
     @GetMapping("detail/{tmNo}")
-    public String tmDetail(Model model, @PathVariable Long tmNo) {
+    public String detail(Model model, @PathVariable Long tmNo) {
         TmResponseDto tm = service.findById(tmNo);
         model.addAttribute("tm", tm);
         model.addAttribute("client", tm.getClient());
@@ -51,8 +50,7 @@ public class TmController {
      * TM 등록 양식
      */
     @GetMapping("register")
-    public void register() {
-    }
+    public void register() { }
 
     /**
      * TM 등록
@@ -68,67 +66,17 @@ public class TmController {
         return "redirect:/tm/list";
     }
 
+    @GetMapping("update/{seq}")
+    public String update(@PathVariable Long seq, Model model) {
+        TmResponseDto tm = service.findById(seq);
+        model.addAttribute("tm", tm);
 
-    /**
-     * TM 등록 - 업체 등록
-     */
-    @PostMapping("register/client")
-    public String registerClient(ClientCreateRequestDto clientDto, RedirectAttributes redirectAttributes) {
-
-        Client client;
-
-        // 고객 회사명으로 조회 후 있으면 UPDATE, 없으면 INSERT
-        /*if (clientService.isExist(clientDto.getCompanyName())) {
-            client = clientService.updateByName(clientDto.getCompanyName(), clientDto);
-        } else {
-            client = clientService.save(clientDto);
-        }*/
-
-        //redirectAttributes.addAttribute("clientSeq", client.getClientSeq());
-
-        return "redirect:/tm/register/tmInfo";
+        return "tm/update";
     }
 
-    /**
-     * TM 등록(상담 정보) 양식
-     */
-    @GetMapping("register/tmInfo")
-    public String tmForm(@ModelAttribute("clientSeq") Long clientSeq, Model model) {
-        model.addAttribute("clientSeq", clientSeq);
-
-        return "tm/register/tmInfo";
-    }
 
     /**
-     * TM 등록(상담 정보) 등록
-     */
-    @PostMapping("register/tmInfo")
-    public String registerTmInfo(TmCreateRequestDto requestDto, @LoginUser SessionUser user) {
-
-        requestDto.setUserSeq(user.getUserSeq());
-        service.save(requestDto);
-
-        return "redirect:/tm/list";
-    }
-
-    /**
-     * TM 등록(매출 정보) 양식
-     */
-    @GetMapping("register/sales")
-    public String registerSales(Model model) {
-        return "tm/register/sales";
-    }
-
-    /**
-     * TM 등록(매출 정보) 등록
-     */
-    @PostMapping("register/sales")
-    public String registerSales() {
-        return "redirect:/tm/list";
-    }
-
-    /**
-     * 대출 정보 등록 화면
+     * [TEST] 대출 정보 등록 화면
      */
     @GetMapping("loan/register")
     public void registerLoanInfo() {
@@ -136,7 +84,7 @@ public class TmController {
     }
 
     /**
-     * 대출 정보 등록
+     * [TEST] 대출 정보 등록
      */
     @PostMapping("loan/register")
     public void registerLoanInfo(TmCreateRequestDto requestDto) {
